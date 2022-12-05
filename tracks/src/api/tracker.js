6fe536369b60e880ccd0f1d5a33589c9a80192e5
Raphaël Chemouni <rapha.chemouni@gmail.com>
2022-12-05 15:57:59 -0500

    Add the token to each Axios request

--- tracks/src/api/tracker.js
+++ tracks/src/api/tracker.js
@@ -1,5 +1,23 @@
 import axios from "axios";
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-export default axios.create({
+
+const instance = axios.create({
   baseURL: 'https://32ed-184-163-110-43.ngrok.io'
 });
+
+instance.interceptors.request.use(
+  async (config) => {
+    const token = await AsyncStorage.getItem('token');
+    if (token) {
+      config.headers.Athorization = `Bearer ${token}`;
+    }
+    return config;
+    // return ne modified config file
+  },
+  (err) => {
+    return Promise.reject(err)
+  }
+);
+
+export default instance;
